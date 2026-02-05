facde164e4d43b1eba33f016a44e0ad9e4d4317b
Samkit jain <55991185+sammkkit@users.noreply.github.com>
2026-02-05 15:16:19 +0530

    Update manifest and ad logic

--- app/src/main/java/com/samkit/costcircle/core/utils/BannerAd.kt
+++ app/src/main/java/com/samkit/costcircle/core/utils/BannerAd.kt
@@ -3,6 +3,7 @@ package com.samkit.costcircle.core.utils
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
@@ -20,4 +21,10 @@ fun BannerAd() {
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BannerAdPreview() {
+    BannerAd()
 }
